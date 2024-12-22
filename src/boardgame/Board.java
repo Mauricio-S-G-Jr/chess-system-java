a245177eb24b82e86ef4ec3b2a4b92283752f21e
Mauricio Gonçalves <mauriciosgjr@proton.me>
2024-12-22 11:13:09 -0300

    Special move - En Passant

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -41,7 +41,7 @@ public class Board {
         piece.position = position;
     }
 
-    public Piece removPiece(Position position) {
+    public Piece removePiece(Position position) {
         if (!positionExists(position))
             throw new BoardException("Position not on the board!");
 
